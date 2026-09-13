a6884c4cad28af0aaa95db8fa44994987680ba99
Luis Souza <luishcs03@gmail.com>
2026-09-13 08:33:28 -0300

    feat: configura Django Admin com inlines, filtros e acoes em lote

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mesa, Comanda, ItemComanda, ItemAdicional
+
+@admin.register(Mesa)
+class MesaAdmin(admin.ModelAdmin):
+    list_display = ('identificacao', 'capacidade', 'ativa', 'esta_ocupada')
+    list_filter = ('ativa',)
+    search_fields = ('identificacao',)
+
+    @admin.display(boolean=True, description='Ocupada')
+    def esta_ocupada(self, obj):
+        return obj.ocupada
+
+class ItemComandaInline(admin.TabularInline):
+    model = ItemComanda
+    extra = 1
+    autocomplete_fields = ('prato', 'combo')
+    fields = ('prato', 'combo', 'quantidade', 'preco_unitario', 'observacao', 'status')
+
+@admin.register(Comanda)
+class ComandaAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'tipo', 'mesa', 'responsavel', 'status', 'valor_total', 'aberta_em',)
+    list_filter = ('status', 'tipo', 'mesa')
+    search_fields = ('codigo', 'responsavel',)
+    readonly_fields = ('codigo', 'aberta_em', 'fechada_em', 'total_pago')
+    date_hierarchy = 'aberta_em'
+    inlines = [ItemComandaInline]
+
+    @admin.display(description='Total')
+    def valor_total(self, obj):
+        return f'R$ {obj.total:.2f}'
+
+class ItemAdicionalInline(admin.TabularInline):
+    model = ItemAdicional
+    extra = 1
+    autocomplete_fields = ('prato',)
+
+@admin.register(ItemComanda)
+class ItemComandaAdmin(admin.ModelAdmin):
+    list_display = ('comanda', 'descricao_produto', 'quantidade', 'preco_unitario', 'status', 'criado_em')
+    list_filter = ('status', )
+    inlines = [ItemAdicionalInline]
+    actions = ['marcar_em_preparo', 'marcar_pronto', 'marcar_entregue']
+
+    @admin.action(description='Marcar como em preparo')
+    def marcar_em_preparo(self, request, queryset):
+        atualizados = queryset.update(status=ItemComanda.Status.EM_PREPARO)
+        self.message_user(request, f'{atualizados} item(ns) em preparo.')
+
+    @admin.action(description='Marcar como Pronto')
+    def marcar_pronto(self, request, queryset):
+        atualizados = queryset.update(status=ItemComanda.Status.PRONTO)
+        self.message_user(request, f'{atualizados} item(ns) pronto(s).')
+
+    @admin.action(description='Marcar como Entregue')
+    def marcar_entregue(self, request, queryset):
+        atualizados = queryset.update(status=ItemComanda.Status.ENTREGUE)
+        self.message_user(request, f'{atualizados} item(ns) entregue(s).')
--- cardapio/admin.py
+++ cardapio/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Prato, Combo, ComboItem
+
+admin.site.site_header = 'Comanda Digital'
+admin.site.site_title = 'Comanda Digital'
+admin.site.index_title = 'Administração do Restaurante'
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ordem', 'eh_adicional')
+    list_editable = ('ordem', 'eh_adicional')
+    search_fields = ('nome',)
+
+@admin.register(Prato)
+class PratoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'categoria', 'disponivel')
+    list_editable = ('preco', 'categoria', 'disponivel')
+    search_fields = ('nome', 'descricao')
+    list_per_page = 25
+
+class ComboItemInline(admin.TabularInline):
+    model = ComboItem
+    extra = 2
+    autocomplete_fields = ('prato',)
+
+@admin.register(Combo)
+class ComboAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'preco_avulso', 'economia', 'disponivel')
+    list_filter = ('disponivel',)
+    search_fields = ('nome', )
+    inlines = [ComboItemInline]
