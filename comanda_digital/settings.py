f5726b301841db59fa4e97d70e0fa3f327765f33
Luis Souza <luishcs03@gmail.com>
2026-09-13 10:20:51 -0300

    chore: configura diretorio de templates e rotas do projeto

--- comanda_digital/settings.py
+++ comanda_digital/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'comanda_digital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
